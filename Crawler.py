ca5abd21e8d5c3201f6334a51a5358d334fca450
PDmmnn <167757590+PDmmnn@users.noreply.github.com>
2024-04-22 11:32:01 +0200

    Create Crawler.py

---
+++ Crawler.py
@@ -0,0 +1,27 @@
+
+import requests from bs4 import BeautifulSoup
+import requests
+url = "https://www.foerderdatenbank.de/FDB/Content/DE/Foerderprogramm/Land/Sachsen/buergschaft-sachsen-beteiligung.html"
+
+response = requests.get(url)
+html_content = response.text
+soup = BeautifulSoup(html_content, 'html.parser')
+
+# Extract all <p> tags with class="content"
+
+Ansprechpunkte = soup.find_all('dd', class ='card')
+Förderarten = soup.find_all('dd')
+#ProgramContents = soup.find_all('div', class_='content')
+
+#Print the text of each paragraph
+for Ansprechpunkt in Ansprechpunkte: 
+  print (Ansprechpunkt.text)
+
+# Print the text of each item
+
+for Förderart in Förderarten:
+  print(Förderart.text)
+
+# for ProgramContent in ProgramContents:
+
+# print(ProgramContent.text)
