622966c4b298b8330b9f5e89974893b495956f24
PDmmnn <167757590+PDmmnn@users.noreply.github.com>
2024-06-17 13:32:26 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('foerderalertForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted!');
+});
+
+function createSearchRequest() {
+    alert('Suchauftrag erstellt!');
+}
