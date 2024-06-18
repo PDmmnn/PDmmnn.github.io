8e78698e0232803ea8e2ccb97144436f2dbb43cd
PDmmnn <167757590+PDmmnn@users.noreply.github.com>
2024-06-18 13:51:49 +0200

    Update suche.js

--- suche.js
+++ suche.js
@@ -16,7 +16,7 @@ fetch('searchterms.json')
 let searchTermsFoerderberechtigt = [];
 fetch('searchTermsFoerderberechtigt.json')
     .then(response => response.json())
-    .then(data => searchTerms = data);
+    .then(data => searchTermsFoerderberechtigt = data);
 
 /*let searchTermsFoerderbereich = [];
 fetch('searchTermsFoerderbereich.json')
