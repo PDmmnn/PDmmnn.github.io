4d1b08c551ba23ce897246e42bea0e26c9b82810
PDmmnn <167757590+PDmmnn@users.noreply.github.com>
2024-06-25 08:53:16 +0200

    Update suche195.js

--- suche195.js
+++ suche195.js
@@ -159,7 +159,7 @@ function formatPercentage(percentageString) {
 }
 
 function search(query) {
-        const apiKey = 'AIzaSyAoJA3vFYtqyije1bB9u8flPdn7d2wkKNk'; // Replace with your actual API key
+        const apiKey = 'AIzaSyDor3KeS2NUadNOejG1-UsJiuksdgA5wZs'; // Replace with your actual API key
         const cx = 'c186732db6bed4a3f'; // Replace with your actual Custom Search Engine ID
         const url = `https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=${cx}&q=${encodeURIComponent(query)}`;
 
