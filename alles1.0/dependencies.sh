bffec919ed86386231394781defaacb0fc4b5aee
PDmmnn <167757590+PDmmnn@users.noreply.github.com>
2024-08-19 08:30:02 +0200

    Delete alles1.0 directory

--- alles1.0/dependencies.sh
+++
@@ -1,5 +0,0 @@
-# initialize Node.js project and install necessary packages
-mkdir backend   # Create a directory for your backend code
-cd backend
-npm init -y     # Initialize npm project
-npm install express mongoose node-cron nodemailer dotenv
